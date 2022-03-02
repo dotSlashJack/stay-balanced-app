de86569d8b87d5e6ce17b0f1682c6a10fc3ccff8
Jack Hester <5445jhes@gmail.com>
2022-03-02 16:48:31 -0500

    Initial commit

---
+++ app/src/main/java/edu/staybalanced/staybalanced/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.staybalanced.staybalanced;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
