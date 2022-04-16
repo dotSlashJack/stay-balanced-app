f4d5d936d6da1c8830b663ef0700e0a744564195
dotSlashJack <5445jhes@gmail.com>
2022-04-16 17:02:47 -0400

    fixes to navigation names and color resource

--- app/src/main/java/edu/staybalanced/staybalanced/Home.java
+++ app/src/main/java/edu/staybalanced/staybalanced/Home.java
@@ -37,7 +37,7 @@ public class Home extends Fragment {
             switch (view1.getId()) {
                 case R.id.home_btnStartExercise:
                     // TODO: Button should go to ExerciseSelector
-                    navController.navigate(NavGraph00Directions.toGyro());
+                    navController.navigate(NavGraph00Directions.toDoExercise());
                     break;
                 case R.id.home_btnAddExercise:
                     navController.navigate(NavGraph00Directions.toNewExercise());
